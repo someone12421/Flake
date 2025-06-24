ea50ef4068f663cd7eaef31f64d57294ce66461b
Someone12421 <randomguy145@protonmail.com>
2025-06-24 03:31:16 +0300

    add droidcam-obs

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -136,6 +136,9 @@
   programs.obs-studio = {
     enable = true;
     enableVirtualCamera = true;
+    plugins = with pkgs.obs-studio-plugins; [
+     droidcam-obs
+    ];
   };
 
   # Before changing this value read the documentation for this option
