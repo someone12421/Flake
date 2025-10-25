28181158fea303a4ada10389f6c32d956c50a27c
Someone12421 <randomguy145@protonmail.com>
2025-10-26 00:50:44 +0300

    readd appimages and add some stuff to nix-ld

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -119,10 +119,13 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  programs.appimage.enable = true;
   programs.nix-ld = {
       enable = true;
       libraries = with pkgs; [
         (pkgs.runCommand "steamrun-lib" {} "mkdir $out; ln -s ${pkgs.steam-run.fhsenv}/usr/lib64 $out/lib")
+        nss
+        nspr
       ];
     };
 
